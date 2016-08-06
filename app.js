34f1b51352a4066692af34aa971293d30c8e03bf
sagittaria <shuyang_x@hotmail.com>
2016-08-06 10:37:05 +0800

    URL路由-分层设计Router

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use('/account',require('./routes/account'));
+//分层设计，自建的路由
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
---
+++ routes/account.js
@@ -0,0 +1,11 @@
+// file: routes/account.js
+var express = require('express');
+var router = express.Router();
+router.get('/login', function(req, res){
+    return res.send('这里将会是登陆页面');
+});
+router.get('/register', function(req, res){
+    return res.send('这里将会是注册页面');
+});
+// 其他的账户控制器
+module.exports = router;
