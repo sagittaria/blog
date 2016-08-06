b9bb3ee0c19bbf425b2cdc4c5ce767fceba3e087
sagittaria <shuyang_x@hotmail.com>
2016-08-06 12:11:13 +0800

    handle favicon.ico

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.set('view engine', 'jade');
 app.use('/account',require('./routes/account'));
 //分层设计，自建的路由
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
