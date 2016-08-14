769c5a91b61223f88ab245674877d87c25e60287
sagittaria <shuyang_x@hotmail.com>
2016-08-14 23:56:39 +0800

    create -user- model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username: {type: String, index: {unique: true}},
+    password: String,
+    avatar: {
+        type: String,
+        default: '/images/default-avatar.jpeg'
+    },
+    title: {
+        type: String,
+        default: '未命名博客'
+    },
+    description: {
+        type: String,
+        default: '博主很懒，还没有添加任何描述……'
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
