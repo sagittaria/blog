31fdf4c34eb559d522d2b0aad9ccbc5a78ebf1ff
sagittaria <shuyang_x@hotmail.com>
2016-08-14 23:57:23 +0800

    add code: handle posted form data

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,6 @@
 // file: routes/account.js
 var express = require('express');
+var User = require('../models/user');
 var router = express.Router();
 router.get('/login', function(req, res){
     return res.send('这里将会是登陆页面');
@@ -18,7 +19,7 @@ router.route('/register')
         if(username.length === 0 || password.length === 0){
             return res.status(400).end('用户名或密码不合法');
         }
-
+/*
         // 将来会在这里检查用户名是否存在，我们先把它设为true
         var usernameExist = true;
 
@@ -29,7 +30,12 @@ router.route('/register')
         // 将来会在这里执行用户、密码的存储
 		
 		res.setHeader('content-type','text/html; charset=UTF-8');
-        res.status(201).end('注册成功');
+        res.status(201).end('注册成功');*/
+		User.create({username: username, password: password},
+			function(err, user) {
+                //if (err) return next(err);    // 交给接下来的错误处理中间件
+                res.status(201).end('注册成功');       // 存储成功
+            });
     });
 
 module.exports = router;
