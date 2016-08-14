44f5ad24f311cad3db69864de9f8fb6d73131378
sagittaria <shuyang_x@hotmail.com>
2016-08-14 22:53:18 +0800

    get rid of warning: mpromise is deprecate

--- mongoose.js
+++ mongoose.js
@@ -1,4 +1,9 @@
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;//百度搜索来的，加了就不再出现“Mongoose: mpromise (mongoose's default promise library) is deprecated, plug in your own promise library instead: http://mongoosejs.com/docs/promises.html”
+/*这是两处参考资料
+http://stackoverflow.com/questions/38138445/node3341-deprecationwarning-mongoose-mpromise
+https://github.com/Automattic/mongoose/wiki/5.0-Deprecation-Warnings
+*/
 mongoose.connect('mongodb://localhost/test');
 
 var Cat = mongoose.model('Cat', { name: String });
